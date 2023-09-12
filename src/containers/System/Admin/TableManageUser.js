ce530871c947f41800e0e1b498e2f69e41bd6373
vinctuan02 <vinctuan02@gmail.com>
2023-09-13 00:02:17 +0700

    test

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -76,7 +76,7 @@ class TableManagerUser extends Component {
 
 const mapStateToProps = state => {
     return {
-        listUsers: state.admin.users
+        listUsers: state.admin.use
     };
 };
 
